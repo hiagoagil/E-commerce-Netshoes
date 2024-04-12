0fca65c69b5f6f2d21492c50b23fca586ae489e5
unknown <gabriellogan.0804@gmail.com>
2024-04-12 15:55:58 -0300

    Refact code netshoes

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,10 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'https://www.netshoes.com.br/'
+    baseUrl: 'https://www.netshoes.com.br/',
+    proxy: 'https://apm.netshoes.com.br' // Add the proxy configuration here
   },
+  viewportWidth: 1440,
+  viewportHeight: 900,
+  chromeWebSecurity: false
 });
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,58 +1,17 @@
-import { faker as fakerJs } from '@faker-js/faker';
-import fakerBr from 'faker-br';
-
-
-
 describe('Cadastro de usuário', () => {
-  
-  const cadastro = {
-    nome: fakerJs.person.firstName(),
-    sobrenome: fakerJs.person.lastName(),
-    email: `${fakerJs.string.alpha(7)}@${Cypress.env('MAILOSAUR_SERVER_ID')}.mailosaur.net`,
-    senha: fakerJs.internet.password({ length: 10 }),
-    dia: fakerJs.string.numeric({ min: 1, max: 31 }),
-    mes: fakerJs.date.month(),
-    ano: fakerJs.string.numeric({ min: 1903, max: 2024 }),
-    rua: fakerJs.location.street(),
-    
-  }
-  
-
-
-
+  beforeEach(() => {
+    cy.intercept('POST', 'https://apm.netshoes.com.br/intake/v2/rum/events', {
+      prevent: true 
+    });
+  });
   it('Realizando cadastro', () => {
-    const cpf = fakerBr.br.cpf();
-
-    cy.visit('auth/login')
-    cy.get('#email').type(cadastro.email)
-    cy.get(':nth-child(2) > .column > .form-group > [data-testid="submitButton"]').click()
-    cy.get('#name').type(cadastro.nome)
-    cy.get('#lastName').type(cadastro.sobrenome)
-    cy.get(':nth-child(1) > .radio > .radio__label').click()
-    cy.get(':nth-child(2) > .radio > .radio__label').click()
-    cy.get('#dayOfBirth').select('20')
-    cy.get('#monthOfBirth').select('Setembro')
-    cy.get('#yearOfBirth').select('1995')
-    cy.get('#cpf').type(cpf)
-    cy.get('#homePhone').type('61986213252')
-    cy.get('#zipCode').type('77720000')
-    cy.get('#streetType').select('Avenida')
-    cy.get('#streetName').type(cadastro.rua)
-    cy.get('#streetNumber').type(cadastro.dia)
-    cy.get('#streetType').select('Avenida')
-    cy.get('#neighborhood').type('Centro')
-    cy.get('#password').type(cadastro.senha)
-    cy.get('#emailNewsletter').uncheck()
-    cy.get(':nth-child(7) > .column > .checkbox > .checkbox__label > .checkbox__input').check()
-    cy.get('[qa-auto="gift-wrapping-select-button"]').click()
-    cy.get('[data-testid="actionEmailButton"]').click()
-    cy.mailosaurGetMessage(Cypress.env('MAILOSAUR_SERVER_ID'), {
-      sentTo: cadastro.email
-    }).then(message => {
-      const confirmationCode = message.html.codes[0].value
-      cy.get(':nth-child(1) > .otp-input').type(confirmationCode)
-    })
-    
-    
-  })
-})
+    cy.generateUserData().then((userData) => {
+      cy.registerUser(userData);
+      cy.enterConfirmationCode(userData.email);
+      cy.get('.status__title').invoke('text').then((text) => {
+        const textoSemEspacos = text.trim();
+        expect(textoSemEspacos).to.eq('Sua conta foi cadastrada!');
+      });
+    });
+  });
+});
